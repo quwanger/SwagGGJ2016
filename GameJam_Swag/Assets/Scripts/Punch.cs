d715d97fabe7b860c74f2170c577b6f192baea07
Paden <paden.shorey@gmail.com>
2016-01-29 23:19:11 -0500

    - added multiplayer capability - added punch - added stun

---
+++ GameJam_Swag/Assets/Scripts/Punch.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Punch : MonoBehaviour {
+
+	public float punchStartTime;
+	private float punchDuration = 0.3f;
+	private float punchRecharge = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time > (punchStartTime + punchDuration)){
+			EndPunch(true);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.GetComponent<PlayerController> ()){
+			//you punched a player
+			PlayerController victim = other.gameObject.GetComponent<PlayerController> ();
+			if(victim.pState == PlayerController.playerState.Idle || victim.pState == PlayerController.playerState.Punching)
+			{
+				victim.Stun();
+				EndPunch(true);
+			}
+			else if(victim.pState == PlayerController.playerState.Carrying || victim.pState == PlayerController.playerState.Throwing)
+			{
+			}
+		}
+		//else if(other.gameObject.GetComponent<MapleLeaf> ())
+		//{
+			//pick up maple leaf
+			//change state to carrying
+		//}
+	}
+
+	void EndPunch(bool toIdle)
+	{
+		if (toIdle) {
+			this.transform.parent.transform.parent.GetComponent<PlayerController> ().pState = PlayerController.playerState.Idle;
+		} else {
+
+		}
+		this.transform.parent.transform.parent.GetComponent<PlayerController> ().punchResetTime = (Time.time + punchRecharge);
+		this.gameObject.SetActive(false);
+	}
+}
