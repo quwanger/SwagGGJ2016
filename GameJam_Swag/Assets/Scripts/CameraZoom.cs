8132c14ca9fe10b9120b6905fad70d6961ac36fc
Dawn <dawn.blairr@gmail.com>
2016-01-29 21:23:50 -0500

    Gitignore update iTween plugin Camera zoom

---
+++ GameJam_Swag/Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraZoom : MonoBehaviour {
+
+	private float originPosition;
+
+	void Start () {
+		originPosition = Camera.main.orthographicSize;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space"))
+			ZoomIn ();
+	}
+
+	void ZoomIn () {
+
+		iTween.ValueTo (Camera.main.gameObject, iTween.Hash ("from", originPosition,
+		                                                     "to", 2f,
+		                                                     "time", 0.5f,
+		                                                     "easetype", iTween.EaseType.easeInExpo,
+		                                                     "onupdate", "UpdateOrthographicCameraSize",
+		                                                     "onupdatetarget", Camera.main.gameObject,
+															 "oncomplete", "ZoomOut"));	
+	}
+	
+	void UpdateOrthographicCameraSize (float size) {
+		Camera.main.orthographicSize = size;
+	}
+
+	void ZoomOut () {
+		iTween.ValueTo (Camera.main.gameObject, iTween.Hash ("from", Camera.main.orthographicSize,
+		                                                     "to", originPosition,
+		                                                     "time", 0.7f,
+		                                                     "easetype", iTween.EaseType.easeOutExpo,
+		                                                     "onupdate", "UpdateOrthographicCameraSize",
+		                                                     "onupdatetarget", Camera.main.gameObject));
+	}
+}
