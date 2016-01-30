fbc9579514aac43b0ffc219b5bc7021a9f14cb44
Dawn <dawn.blairr@gmail.com>
2016-01-29 22:52:36 -0500

    Camera zoom now moves camera position

--- GameJam_Swag/Assets/Scripts/CameraZoom.cs
+++ GameJam_Swag/Assets/Scripts/CameraZoom.cs
@@ -3,39 +3,74 @@ using System.Collections;
 
 public class CameraZoom : MonoBehaviour {
 
-	private float originPosition;
+	private float defaultSize;
+	private float defaultPosition;
+
+	public GameObject character;
 
 	void Start () {
-		originPosition = Camera.main.orthographicSize;
+		defaultSize = Camera.main.orthographicSize;
+		defaultPosition = this.transform.position.x;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown ("space"))
+		if (Input.GetKeyDown ("space")) {
 			ZoomIn ();
+		}
 	}
 
 	void ZoomIn () {
-
-		iTween.ValueTo (Camera.main.gameObject, iTween.Hash ("from", originPosition,
+		iTween.ValueTo (this.gameObject, iTween.Hash ("from", defaultSize,
 		                                                     "to", 2f,
 		                                                     "time", 0.5f,
 		                                                     "easetype", iTween.EaseType.easeInExpo,
 		                                                     "onupdate", "UpdateOrthographicCameraSize",
-		                                                     "onupdatetarget", Camera.main.gameObject,
-															 "oncomplete", "ZoomOut"));	
+		                                                     "onupdatetarget", this.gameObject,
+															 "oncomplete", "ZoomOut"));
+
+		iTween.ValueTo (this.gameObject, iTween.Hash ("from", defaultPosition,
+		                                                     "to", character.transform.position.x,
+		                                                     "time", 0.5f,
+		                                                     "easetype", iTween.EaseType.easeInExpo,
+		                                                     "onupdate", "UpdateCameraPosition",
+		                                                     "onupdatetarget", this.gameObject,
+		                                                     "oncomplete", "moveToOrigin"));
 	}
 	
 	void UpdateOrthographicCameraSize (float size) {
-		Camera.main.orthographicSize = size;
+		Camera.main.orthographicSize = size; 
+	}
+
+	void UpdateCameraPosition (float position) {
+		Debug.Log (position);
+		this.transform.position = new Vector3(position, this.transform.position.y, this.transform.position.z);
+	}
+
+	void moveToOrigin () {
+		StartCoroutine(Wait(1));
 	}
 
 	void ZoomOut () {
-		iTween.ValueTo (Camera.main.gameObject, iTween.Hash ("from", Camera.main.orthographicSize,
-		                                                     "to", originPosition,
-		                                                     "time", 0.7f,
-		                                                     "easetype", iTween.EaseType.easeOutExpo,
-		                                                     "onupdate", "UpdateOrthographicCameraSize",
-		                                                     "onupdatetarget", Camera.main.gameObject));
+		StartCoroutine(Wait(1));
 	}
+
+	IEnumerator Wait(int timeInSeconds) {
+		yield return new WaitForSeconds(timeInSeconds);
+
+		iTween.ValueTo (this.gameObject, iTween.Hash ("from", Camera.main.orthographicSize,
+		                                              "to", defaultSize,
+		                                              "time", 0.7f,
+		                                              "easetype", iTween.EaseType.easeOutExpo,
+		                                              "onupdate", "UpdateOrthographicCameraSize",                                           
+		                                              "onupdatetarget", this.gameObject));
+
+		iTween.ValueTo (this.gameObject, iTween.Hash ("from", this.transform.position.x,
+		                                              "to", defaultPosition,
+		                                              "time", 0.7f,
+		                                              "easetype", iTween.EaseType.easeOutExpo,
+		                                              "onupdate", "UpdateCameraPosition",
+		                                              "onupdatetarget", this.gameObject));
+	}
+	
 }
