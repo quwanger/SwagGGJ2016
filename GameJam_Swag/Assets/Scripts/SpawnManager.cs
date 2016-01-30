eb38ecb7b324c2990323011e18cac1648b97ebf2
WHOAMI <WHOAMI@gmail.com>
2016-01-29 23:54:55 -0500

    - Maple Leaf prefab - Spawn manager - Maple leaf asset

---
+++ GameJam_Swag/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnManager : MonoBehaviour {
+
+	public GameObject mapleLeaf;
+
+	// Use this for initialization
+	void Start () {
+		Vector3 position = new Vector3 (Random.Range (-10.0F, 10.0F), Random.Range (-10F, 10.0F), 0);
+		Instantiate (mapleLeaf, position, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
