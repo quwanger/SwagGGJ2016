c4f3adb338080366302af86d05ba9fe272d80faf
Paden <paden.shorey@gmail.com>
2016-01-30 00:44:01 -0500

    Starting the level layout

---
+++ GameJam_Swag/Assets/Scripts/BaseManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseManager : MonoBehaviour {
+	public GameObject order;
+
+	private PlayerController playerPosition;
+	public CircleCollider2D playerCollider;
+	public SpriteRenderer render;
+	
+	// Use this for initialization
+	void Start () {
+		render = order.GetComponent<SpriteRenderer> ();
+		//
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.GetComponent<PlayerController> ()) {
+			render.color = new Color (0.5f, 0.5f, 0.5f);
+		}
+	}
+}
