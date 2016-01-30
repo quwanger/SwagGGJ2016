7d12372912abfe1b847a445a5c891f1484682046
Paden <paden.shorey@gmail.com>
2016-01-29 20:36:35 -0500

    - added a playerController and a test scene

---
+++ GameJam_Swag/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	private Vector2 movementVector;
+
+	public float movementSpeed = 4f;
+	public float dragConstant = 0.09f;
+
+	// Use this for initialization
+	void Start () {
+		rb = this.gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		movementVector.x = Input.GetAxis("LeftJoystickX") * movementSpeed;
+		movementVector.y = Input.GetAxis("LeftJoystickY") * movementSpeed * -1;
+
+		this.gameObject.transform.localEulerAngles = new Vector3(0, 0, Mathf.Rad2Deg * (Mathf.Atan2 (movementVector.y, movementVector.x)) - 90f);
+
+		movementVector.x = Mathf.Lerp (rb.velocity.x, movementVector.x, dragConstant);
+		movementVector.y = Mathf.Lerp (rb.velocity.y, movementVector.y, dragConstant);
+
+		rb.velocity = movementVector;
+		//rb.AddForce (movementVector * movementSpeed);
+	}
+}
